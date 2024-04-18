132904c8c6afbd4ecce945051d20b4dd1b494ada
e-basoglu <151559231+e-basoglu@users.noreply.github.com151559231+e-basoglu@users.noreply.github.com>
2024-04-18 02:56:59 +0200

    to do update

--- src/Todo.js
+++ src/Todo.js
@@ -1,46 +1,83 @@
 import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import "./style.css";
+import logo from "./logo.png"; // Import the logo image
 
 function uniqueId() {
-  return Math.floor(Math.random() * 10);
+  return Math.floor(Math.random() * 10000);
 }
 
 function Todo() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
+  const [selectedDate, setSelectedDate] = useState(null);
 
   const handleSubmit = () => {
-    setTodos((todos) =>
-      todos.concat({
-        text: input,
-        id: uniqueId(),
-      })
-    );
-    setInput("");
+    if (input.trim() !== "" && selectedDate !== null) {
+      setTodos((prevTodos) =>
+        prevTodos.concat({
+          text: input,
+          id: uniqueId(),
+          date: selectedDate,
+        })
+      );
+      setInput("");
+      setSelectedDate(null);
+    } else {
+      alert("Please enter a task and select a date.");
+    }
   };
 
   const removeTodo = (id) =>
-    setTodos((todos) => todos.filter((t) => t.id !== id));
-
-
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
 
   return (
     <div className="container">
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder="Add a new task"
-      />
+      <img src={logo} alt="Todo App Logo" className="logo" /> {/* Add logo here */}
+      <h1>Welcome to My Todo App</h1>
+      <p>
+        This simple todo app helps you keep track of your tasks. Just enter a
+        task in the input field, select a date, and click "Add Task". Click "Remove" to delete it from
+        the list.
+      </p>
 
-      <button onClick={handleSubmit}>Create</button>
+      <div className="input-container">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Add a new task"
+        />
+        <DatePicker
+          selected={selectedDate}
+          onChange={(date) => setSelectedDate(date)}
+          placeholderText="Select Date"
+          dateFormat="MM/dd/yyyy"
+          minDate={new Date()}
+        />
+        <button onClick={handleSubmit}>Add Task</button>
+      </div>
 
       <ul className="todo-list">
-        {todos.map(({ text, id }) => (
+        {todos.map(({ text, id, date }) => (
           <li key={id} className="todo">
-            <span>{text}</span>
+            <div className="todo-content">
+              <input
+                type="text"
+                value={text}
+                readOnly
+                className="todo-input"
+              />
+              <DatePicker
+                selected={date}
+                readOnly
+                dateFormat="MM/dd/yyyy"
+                className="todo-date"
+              />
+            </div>
             <button className="erase" onClick={() => removeTodo(id)}>
-              x
+              Remove
             </button>
           </li>
         ))}
